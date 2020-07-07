3103a1636adcd12d87d18ab5f0bce299ae55d186
code404it <jessikahd.smith@gmail.com>
2020-07-07 15:37:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 class App extends Component {
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json)
+        .then(data => {
+            this.setState({
+                peopleInSpace: data.people
+            })
+        })
+    }
     render() {
         return (
             <div>
-                componentDidMount() {
-                    fetch('http://api.open-notify.org/astros.json')
-                        .then(response => response.json())
-                        .then(data => { 
-                            this.setState({
-                                peopleInSpace: data.people
-                            })
-                        })
-                }  
             </div>
         );
     }
